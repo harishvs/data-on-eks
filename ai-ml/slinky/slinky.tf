16070ec57b7b51fc1789aef1fedff1787861f0e0
Harish Rao <harisvs@amazon.com>
2024-12-27 22:42:22 -0800

    WIP - the slurm controller is not creating

---
+++ ai-ml/slinky/slinky.tf
@@ -0,0 +1,72 @@
+# Create namespace for Slinky
+resource "kubernetes_namespace" "slinky" {
+  metadata {
+    name = "slinky"
+  }
+}
+
+# Create a local file resource to store the values.yaml content
+data "http" "values_operator" {
+  url = "https://raw.githubusercontent.com/SlinkyProject/slurm-operator/refs/tags/v0.1.0/helm/slurm-operator/values.yaml"
+}
+
+# Install Slurm operator using Helm
+resource "helm_release" "slurm_operator" {
+  name             = "slurm-operator"
+  repository       = "oci://ghcr.io/slinkyproject/charts"
+  chart            = "slurm-operator"
+  version          = "0.1.0"
+  namespace        = kubernetes_namespace.slinky.metadata[0].name
+  create_namespace = true
+
+  # Use the values from the downloaded file
+  values = [
+    data.http.values_operator.response_body
+  ]
+
+  depends_on = [
+    kubernetes_namespace.slinky
+  ]
+
+  # Add timeout in case installation takes longer
+  timeout = 900 # 15 minutes
+}
+
+# Create namespace for Slurm
+resource "kubernetes_namespace" "slurm" {
+  metadata {
+    name = "slurm"
+  }
+}
+
+# Fetch the values.yaml content
+data "http" "values_slurm" {
+  url = "https://raw.githubusercontent.com/SlinkyProject/slurm-operator/refs/tags/v0.1.0/helm/slurm/values.yaml"
+}
+
+# Install Slurm using Helm
+resource "helm_release" "slurm" {
+  name             = "slurm"
+  repository       = "oci://ghcr.io/slinkyproject/charts"
+  chart            = "slurm"
+  version          = "0.1.0"
+  namespace        = kubernetes_namespace.slurm.metadata[0].name
+  create_namespace = true
+
+  values = [
+    data.http.values_slurm.response_body
+  ]
+
+  # Configure storage class
+  set {
+    name  = "controller.persistence.storageClass"
+    value = "fsx"
+  }
+
+  depends_on = [
+    kubernetes_namespace.slurm,
+    helm_release.slurm_operator
+  ]
+
+  timeout = 900
+}
