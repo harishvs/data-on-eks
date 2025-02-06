38e712adfd61aeadb6dec09905ed894644d33c14
Harish Rao <harisvs@amazon.com>
2025-02-05 20:51:11 -0800

    small changes to shello world

--- ai-ml/slinky/submit_hello.sh
+++ ai-ml/slinky/submit_hello.sh
@@ -2,15 +2,11 @@ cat >hello <<EOD
 #!/bin/bash
 #SBATCH --job-name=test_nodes
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --time=00:05:00
 #SBATCH --output=/data/fsx/test_nodes_%j.out
 #SBATCH --error=/data/fsx/test_nodes_%j.err
-echo "SLURM_JOB_ID: $SLURM_JOB_ID"
-echo "SLURM_NODELIST: $SLURM_NODELIST"
 srun hostname
-srun free -h
-srun pwd
 EOD
 
 chmod u+x hello
